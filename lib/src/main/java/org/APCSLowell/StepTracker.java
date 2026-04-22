c4cd38175b87531eaf0b9913dca675e09c60fbaf
DONOWT <selee3@s.sfusd.edu>
2026-04-22 16:13:47 +0000

    Done

--- lib/src/main/java/org/APCSLowell/StepTracker.java
+++ lib/src/main/java/org/APCSLowell/StepTracker.java
@@ -1,8 +1,26 @@
 package org.APCSLowell;
 
 public class StepTracker {
-    public StepTracker(int activityThreshold) {}
-    void addDailySteps(int stepCount) {}
-    int activeDays() {}
-    double averageSteps() {}
+    private int steps; 
+    private int min; 
+    private int active; 
+    private int days;
+    public StepTracker(int activityThreshold) {
+        min = activityThreshold;
+        steps = 0;
+        active = 0;
+        days = 0;
+    }
+    public void addDailySteps(int stepCount) {
+        if (stepCount >= min) {active++;}
+        steps = steps + stepCount;
+        days++;
+    }
+    public int activeDays() {
+        return active;
+    }
+    public double averageSteps() {
+        if (days != 0) {return (double)steps/(double)days;}
+        return 0;
+    }
 }
